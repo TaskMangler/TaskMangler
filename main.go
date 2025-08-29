0e321cbb456c45ed885693042d9e9050bd187f22
vcokltfre <vcokltfre@gmail.com>
2025-08-29 14:16:04 +0100

    feat: setup go project

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	if err := godotenv.Load(); err != nil {
+		if os.IsNotExist(err) {
+			logrus.Warn("No .env file not found, relying on environment variables")
+		} else {
+			logrus.Error("Error loading .env file:", err)
+		}
+	}
+
+	if os.Getenv("ENV") == "production" {
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+		logrus.SetLevel(logrus.InfoLevel)
+		logrus.Info("TaskMangler running in production mode")
+	} else {
+		logrus.SetLevel(logrus.DebugLevel)
+		logrus.Info("TaskMangler running in development mode")
+	}
+}
+
+func main() {}
